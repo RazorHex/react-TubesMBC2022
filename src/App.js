5f5e67454f434067caed4493e6a91a54af9033c7
RazorHex <ahda043@gmail.com>
2022-12-11 15:47:08 +0700

    Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Foot from './Components/Footer';
+import NavbarAll from './Components/Static/Navbar';
+import Foot from './Components/Static/Footer';
 import Home from './Components/Home';
 import Dosen from './Components/Dosen';
-import NavbarAll from './Components/Navbar';
 
 import { Routes, Route } from 'react-router-dom';
 
